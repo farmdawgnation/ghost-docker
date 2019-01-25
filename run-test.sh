96da9ef3765f9b600c4c238bedf450d54cb40813
Matt Farmer <matt@frmr.me>
2019-01-25 11:17:57 -0500

    Tweak test script to show build output

--- run-test.sh
+++ run-test.sh
@@ -7,10 +7,23 @@ echo "Starting goss container"
 GOSSIMAGE="aelsabbahy/goss"
 GOSSCONTAINER=$(docker run -d $GOSSIMAGE)
 
+echo "Goss container is $GOSSCONTAINER"
+
+echo "Building ghost container"
+
+docker build .
+
+echo "Determining ghost container id"
+
+IMAGE=$(docker images -q | head -n 1)
+
+echo "Pretty sure the ghost image is $IMAGE"
+
 echo "Starting ghost container"
 
-IMAGE=$(docker build . -q)
 GHOSTCONTAINER=$(docker run --rm --volumes-from $GOSSCONTAINER --mount type=bind,src=$(pwd)/goss.yaml,dst=/goss.yaml -d $IMAGE)
+
+echo "Ghost container is $GHOSTCONTAINER"
 
 echo "Giving ghost 20 seconds to start"
 
